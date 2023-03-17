4ed450024cb05bb9814df9bdb7f0e5d34aad3a12
artembeluj <artembeluj@gmail.com>
2023-03-17 17:29:36 +0200

    initial commit

---
+++ websocket-basic-example/app.js
@@ -0,0 +1,20 @@
+const ws = new require("ws");
+
+const wsServer = new ws.Server({port: 5000});
+
+const sockets = [];
+
+wsServer.on("connection", (socket)=> {
+    console.log("New frontend connect");
+    console.log(socket);
+    sockets.push(socket);
+    setTimeout(() => {
+        socket.send("Welcome to websocket server");
+    }, 3000);
+
+    sockets.forEach(item => {
+        if(item !== socket) {
+            item.send("New member connect")
+        }
+    })
+});
